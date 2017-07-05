1d8e8fda225a2d1e10d8cbbf0f29e63224a42b3e
Anton Nayshtut <tonynays@gmail.com>
2017-07-05 10:51:34 +0300

    Trivial: pylint comments removed

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from bot import create_app
 import config
 
-# pylint: disable=C0103
 app = create_app(config)
-# pylint: enable=C0103
 
 # This is only used when running locally
 if __name__ == '__main__':
