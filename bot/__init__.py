4efe2cfe2487ce4eaa81aa52163ea556d9234661
Anton Nayshtut <tonynays@gmail.com>
2017-07-05 08:05:34 +0300

    Logger configuration

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,19 +2,25 @@
 Locano Facebook bot implementation
 """
 import logging
-
 from flask import Flask
 from bot.webhook import botbp
+from bot.config import CONFIG
 
 BOT_ROOT = '/bot'
+
+def config_logger(app):
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(CONFIG['LOGFMT'])
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    if app.debug:
+        logger.setLevel(logging.DEBUG)
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
-
-    # Configure logging
-    if app.config['DEBUG']:
-        logging.basicConfig(level=logging.DEBUG)
+    config_logger(app)
 
     logging.info("Registering App: %s", __name__)
 
