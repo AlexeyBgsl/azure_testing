d4392c50897e919bf480f5d3da9948228f707185
Anton Nayshtut <tonynays@gmail.com>
2017-07-13 10:02:13 +0300

    Bot: Steps and CTA based chat

--- bot/chat.py
+++ bot/chat.py
@@ -1,12 +1,106 @@
 import logging
 from abc import ABC, abstractmethod
-from enum import Enum
 from fbmq import QuickReply
 from bot.db_datastore import BasicEntry, BasicTable
 
 
 CHAT_CLB_ID = 'CHAT_CLB'
 START_ACTION = 'Start'
+
+
+class ClassCollection(object):
+    def __init__(self):
+        self.subs = {}
+
+    def register(self, scls):
+        self.subs[scls.__name__] = scls
+        return scls
+
+    def cls(self, class_name):
+        return self.subs[class_name] if class_name in self.subs else None
+
+    def instantiate(self, class_name, *args, **kwargs):
+        scls = self.cls(class_name)
+        return scls(*args, **kwargs) if scls else None
+
+
+class Hierarchy():
+    DELIMITER = '.'
+
+    @classmethod
+    def from_string(cls, path):
+        return cls(path.split(cls.DELIMITER))
+
+    def __init__(self, path=[]):
+        self.path = path
+
+    @property
+    def string(self):
+        path = ''
+        for p in self.path:
+            if path == '':
+                path = p
+            else:
+                path += self.DELIMITER + p
+        return path
+
+    @property
+    def is_empty(self):
+        return len(self.path) == 0
+
+    @property
+    def nof_levels(self):
+        return len(self.path)
+
+    def get_current_level(self):
+        return self.path[self.nof_levels - 1]
+
+    def level_up(self, next_level):
+        self.path.append(next_level)
+
+    def level_down(self):
+        return self.path.pop()
+
+    def __str__(self):
+        return self.string
+
+
+class CallToAction(object):
+    def __init__(self, title, action_id, class_name):
+        self.title = title
+        self.action_id = action_id
+        self.class_name = class_name
+
+
+class BackCallToAction(CallToAction):
+    CLS_NAME = 'BackCTAClass'
+    ACTION_ID = 'BackCTAAction'
+
+    def __init__(self, title='Back'):
+        super().__init__(title, self.ACTION_ID, self.CLS_NAME)
+
+
+class BasicChatStep(ABC):
+    CTA = []
+
+    @classmethod
+    def name(cls):
+        return cls.__name__
+
+    @classmethod
+    def on_action_done(cls, cta, event):
+        pass
+
+    @classmethod
+    def on_action(cls, action_id, event):
+        logging.debug("%s: on_action: %s arrived", cls.name(), action_id)
+        for cta in cls.CTA:
+            if cta.action_id == action_id:
+                cls.on_action_done(cta, event)
+                logging.debug("%s: next CTA: %s", cls.name(), cta.class_name)
+                return cta.class_name
+        return None
+
 
 class ChatTable(BasicTable):
     def __init__(self):
@@ -15,18 +109,43 @@ class ChatTable(BasicTable):
     def by_fbid(self, fbid):
         return self.simple_query(fbid=fbid)
 
+
+step_collection = ClassCollection()
+
+
+@step_collection.register
+class RootChatStep(BasicChatStep):
+    CTA = [
+        CallToAction('Channels', 'RtChls', 'FirstChannelsChatStep'),
+        CallToAction('Announcements', 'RtAnns', 'FirstAnnouncementsChatStep'),
+        CallToAction('Help', 'RtHelp', 'FirstHelpChatStep'),
+    ]
+
+
+@step_collection.register
+class FirstChannelsChatStep(BasicChatStep):
+    CTA = [
+        CallToAction('List My Channels', 'ChList', 'FirstChannelsChatStep'),
+        CallToAction('Subscribe', 'ChSub', 'FirstAnnouncementsChatStep'),
+        CallToAction('Unsubscribe', 'ChUnSub', 'FirstHelpChatStep'),
+        BackCallToAction()
+    ]
+
+
+@step_collection.register
+class FirstAnnouncementsChatStep(BasicChatStep):
+    pass
+
+
+bot_collection = ClassCollection()
+
+
 class BaseChat(BasicEntry):
     chats = ChatTable()
-    subs = {}
-
-    @classmethod
-    def addsub(cls, scls):
-        cls.subs[scls.__name__] = scls
-        return scls
 
     @classmethod
     def by_db_entity(cls, page, entity):
-        i = cls.subs[entity['class_name']](page)
+        i = bot_collection.instantiate(entity['class_name'], page)
         i.from_entity(entity)
         return i
 
@@ -85,8 +204,8 @@ class BaseChat(BasicEntry):
     def class_name(self):
         return type(self).__name__
 
-    def __init__(self, page, fbid=None):
-        super().__init__(self.chats)
+    def __init__(self, page, fbid=None, entity=None):
+        super().__init__(self.chats, entity=entity)
         self.page = page
         self.add_db_field('fbid', fbid)
         self.add_db_property('class_name')
@@ -103,30 +222,64 @@ class BaseChat(BasicEntry):
         pass
 
 
-@BaseChat.addsub
-class SelectChannelActionChat(BaseChat):
-    start_actions = dict(
-        ChList = 'List My Channels',
-        ChSub = 'Subscribe',
-        ChUnSub = 'Unsubscribe',
-        ChCancel = 'Cancel'
-    )
+@bot_collection.register
+class BotChat(BaseChat):
+    @staticmethod
+    def root_menu():
+        return RootChatStep.CTA
 
-    def on_action_start(self):
+    def __init__(self, page, fbid=None, hierarchy=None):
+        super().__init__(page, fbid)
+        h = hierarchy if hierarchy else str(Hierarchy(['RootChatStep']))
+        self.add_db_field('hierarchy', h)
+
+    @property
+    def hobj(self):
+        return Hierarchy.from_string(self.hierarchy)
+
+    def _ask_for_cta(self, cls):
         quick_replies = []
-        for k, v in type(self).start_actions.items():
-            quick_replies.append(QuickReply(title=v, payload=self.format_callback_payload(k)))
+        for c in cls.CTA:
+            quick_replies.append(
+                QuickReply(title=c.title,
+                           payload=self.format_callback_payload(c.action_id)))
         self.page.send(self.fbid,
                        "What do you want to do next?",
                        quick_replies=quick_replies,
                        metadata="DEVELOPER_DEFINED_METADATA")
+
+    @property
+    def current_level_cls(self):
+        class_name = self.hobj.get_current_level()
+        return step_collection.cls(class_name)
+
+    def start(self, action_id, event):
+        self.on_user_action(action_id, event)
+
+    def level_up(self, class_name):
+        cta_cls = step_collection.cls(class_name)
+        if cta_cls:
+            self.hierarchy = str(self.hobj.level_up(class_name))
+            self.save()
+            self._ask_for_cta(cta_cls)
+
+    def level_down(self):
+        hobj = self.hobj
+        assert hobj.nof_levels > 1, "%s: there's no level down" % str(self.hierarchy)
+        self.hierarchy = str(hobj.level_down())
         self.save()
+        if hobj.nof_levels > 1:
+            self._ask_for_cta(self.current_level_cls)
+        else:
+            logging.warning("%s: rolled back to menu", self.class_name)
 
     def on_user_action(self, action_id, event):
-        if action_id == START_ACTION:
-            self.on_action_start()
+        if action_id == BackCallToAction.ACTION_ID:
+            self.level_down()
         else:
-            logging.warning("on_user_action: %s arrived", action_id)
+            cls = self.current_level_cls
+            next_level_class_name = cls.on_action(action_id, event)
+            self.level_up(next_level_class_name)
 
 
 def chat_clb_handler(page, payload, event):
--- bot/page.py
+++ bot/page.py
@@ -3,12 +3,9 @@ import fbmq
 import functools
 from bot.config import CONFIG
 from bot.db_datastore import Users, User
-from bot.chat import CHAT_CLB_ID, chat_clb_handler, SelectChannelActionChat
+from bot.chat import CHAT_CLB_ID, chat_clb_handler, BotChat
 
 MENU_ID = "MENU"
-HELP_ID = "HELP"
-CHAN_ID = "CHANNEL"
-ANNS_ID = "ANNOUNCEMENT"
 
 HELP_MESSAGE = ("This is Locano. We help you to make and receive "
                 "announcements")
@@ -75,13 +72,10 @@ class BotPage(fbmq.Page):
         super().__init__(CONFIG['ACCESS_TOKEN'])
         self.greeting("Hi {{user_first_name}}, welcome to Locano Chatbot!")
         self.show_starting_button("Show Help")
-        self.show_persistent_menu([
-            fbmq.Template.ButtonPostBack('Channel',
-                                         MENU_ID + '/' + CHAN_ID),
-            fbmq.Template.ButtonPostBack('Announcement',
-                                         MENU_ID + '/' + ANNS_ID),
-            fbmq.Template.ButtonPostBack('Help',
-                                         MENU_ID + '/' + HELP_ID), ])
+        buttons = []
+        for b in BotChat.root_menu():
+            buttons.append(fbmq.Template.ButtonPostBack(b.title, MENU_ID + '/' + b.action_id))
+        self.show_persistent_menu(buttons)
 
     def _user_from_fb_profile(self, fbid):
         user = User.create(self.users, self.get_user_profile(fbid))
@@ -149,14 +143,7 @@ class BotPage(fbmq.Page):
         sender_id = event.sender_id
         item_id = payload.split('/')[1]
         logging.debug("[U#%s] [on_menu] %s", sender_id, item_id)
-        if item_id == CHAN_ID:
-            s = SelectChannelActionChat(page, fbid=sender_id)
-            s.start()
-            pass
-        elif item_id == ANNS_ID:
-            pass
-        else:
-            self.send(sender_id, HELP_MESSAGE)
+        BotChat(page, fbid=sender_id).start(item_id, event)
 
 
 page = BotPage()
