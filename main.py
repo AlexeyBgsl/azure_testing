e772cb8069d4dd9b3044f5cf8e3d2b639f3d943c
Anton Nayshtut <tonynays@gmail.com>
2017-08-02 19:50:57 +0300

    Azure deployment files

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import config
 
 app = create_app(config)
 
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
 # This is only used when running locally
 if __name__ == '__main__':
     app.run()
