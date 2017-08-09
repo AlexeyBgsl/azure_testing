8a889a841c53db63f2a3bb6b0f38fd05b10ed9fd
Anton Nayshtut <tonynays@gmail.com>
2017-08-09 16:24:52 +0300

    Bot: BotRef fixed

--- bot/chat.py
+++ bot/chat.py
@@ -564,28 +564,30 @@ class BotRef(object):
         for k in kwargs:
             if ref != '':
                 ref += cls.PARAMS_DELIMITER
-            ref += k + cls.PARAMS_DELIMITER + kwargs[k]
+            ref += k + cls.KEYVAL_DELIMITER + kwargs[k]
         return ref
 
     @classmethod
-    def get_params(self, ref):
+    def get_params(cls, ref):
         d = {}
-        params = ref.split(self.PARAMS_DELIMITER)
+        params = ref.split(cls.PARAMS_DELIMITER)
         for p in params:
-            v = p.split(self.KEYVAL_DELIMITER)
+            v = p.split(cls.KEYVAL_DELIMITER)
             if v and len(v) == 2:
                 d[v[0]] = v[1]
         return d
 
-    def __init__(self, ref=None):
+    def __init__(self, ref=None, **kwargs):
         self.params = self.get_params(ref) if ref else {}
+        if kwargs:
+            self.add_params(**kwargs)
 
-    def add_params(self, params):
-        self.params.update(params)
+    def add_params(self, **kwargs):
+        self.params.update(**kwargs)
 
     @property
     def ref(self):
-        return self.get_ref(self.params)
+        return self.get_ref(**self.params)
 
     def __str__(self):
         return self.ref
