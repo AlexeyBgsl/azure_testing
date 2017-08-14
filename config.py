db6fd2aa8993effbbd1018549d0354dfea4246d3
Anton Nayshtut <tonynays@gmail.com>
2017-08-14 17:09:41 +0300

    Config: always DEBUG

--- config.py
+++ config.py
@@ -4,12 +4,10 @@ App Configuration
 import os
 
 PRODUCTION_APP_ENV = (os.getenv('PRODUCTION_APP_ENV', '') == '1')
-
+DEBUG = os.getenv('DEBUG', True)
 
 if PRODUCTION_APP_ENV:
     # Production
-    DEBUG = False
     TESTING = False
 else:
-    DEBUG = True
     TESTING = False
