0da731912e7f3d21ef72b06c8c5b9f8ea8c35117
Anton Nayshtut <tonynays@gmail.com>
2017-08-15 12:01:16 +0300

    Config: TESTING taken from env

--- config.py
+++ config.py
@@ -6,11 +6,5 @@ import os
 PRODUCTION_APP_ENV = (os.getenv('PRODUCTION_APP_ENV', '') == '1')
 DEBUG = os.getenv('DEBUG', True)
 APP_LOG_FILE = os.getenv('APP_LOG_FILE', '')
-
-if PRODUCTION_APP_ENV:
-    # Production
-    TESTING = False
-else:
-    TESTING = False
-
+TESTING = os.getenv('TESTING', False)
 LOGFMT='[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s'
